e9c09039186ee304cca71ed9595ae4e85697e3c9
Adeline Lim <adelinelim07@gmail.com>
2019-12-07 16:08:18 +0800

    added seed data

--- seedData.js
+++ seedData.js
@@ -1,4 +1,4 @@
-//trip
+const tripCardData=
 [
     {
         title: "Malaysia", 
@@ -16,15 +16,16 @@
         startDate: 08-08-2019,
         endDate: 10-08-2019
     }
-]
+];
 
-//ideas
+const ideaCardData=
 [
     {
         title: "TIDO Penang D1", 
         description: "Accom for day 1", 
         location: "Penang", 
         image: "https://lh3.googleusercontent.com/p/AF1QipPQk0ikt5EQMw2DA5JpcfcRLFzP128jsPsdYyXQ=w592-h404-n-k-rw-no-v1",
+        url:
         comments: ["comment1", "comment2"],  
         category: "Accomodation",
         trip: //put in the trip id,
@@ -34,6 +35,7 @@
         description: "Accom for day 2", 
         location: "Malacca", 
         image: "https://lh3.googleusercontent.com/proxy/XVj-r_nS4n1NgDOsfeRc_DaMO8PFIvE4qs2EvZYcB_HZUkHMZbdOLH0fI8LPKRwKyhZmkk2rH7Za7GPr6XFvMYZD8l-PSgXvhDVJZZe1QWFqetR3FBiUR4kMgBDpyaAEbCz15d29_lznfQiRywB6nlq6Os-g7tA=w592-h404-n-k-rw-no-v1",
+        url: "https://www.agoda.com/en-gb/pages/agoda/default/DestinationSearchResult.aspx?selectedproperty=5903367&city=15121&hid=5903367&site_id=1830014&tag=62e3f967-9ab4-2742-efb7-7cce5202a54f&device=c&network=g&adid=365528788483&rand=8810423548882883564&expid=&adpos=1t1&gclid=CjwKCAiAlajvBRB_EiwA4vAqiOy4nH9HDT7FUH8hcFd0dq1SRc4gWoDvd-XYdw4-XGlQLYfOaGIsExoCXvAQAvD_BwE&checkin=2020-01-24&checkout=2020-01-27&adults=4&children=0&childs=0&rooms=2&los=3",
         comments: ["comment1", "comment2"],  
         category: "Accomodation",
         trip: //put in the trip id,
@@ -42,17 +44,42 @@
         title: "Malacca Land Transport", 
         description: "Land Transport", 
         location: "Malacca", 
-        image: "https://lh3.googleusercontent.com/proxy/XVj-r_nS4n1NgDOsfeRc_DaMO8PFIvE4qs2EvZYcB_HZUkHMZbdOLH0fI8LPKRwKyhZmkk2rH7Za7GPr6XFvMYZD8l-PSgXvhDVJZZe1QWFqetR3FBiUR4kMgBDpyaAEbCz15d29_lznfQiRywB6nlq6Os-g7tA=w592-h404-n-k-rw-no-v1",
+        image: "https://www.easybook.com/images/article/bus-from-melaka-to-singapore.jpg",
+        url: "http://www.porterxpress.com/malaysia-travel/visit-malacca-and-transport.html",
+        comments: ["comment1", "comment2"],  
+        category: "Transport",
+        trip: //put in the trip id,
+    },
+    {
+        title: "Jonker Street", 
+        description: "Must Go", 
+        location: "Malacca", 
+        image: "http://static.asiawebdirect.com/m/kl/portals/malacca-ws/homepage/jonker-street/pagePropertiesImage/jonker-street.jpg",
+        url: "http://www.malacca.ws/jonker-street/",
+        comments: ["comment1", "comment2"],  
+        category: "Places of Interest",
+        trip: //put in the trip id,
+    },
+    {
+        title: "Halong Bay", 
+        description: "UNESCO", 
+        location: "Vietnam", 
+        image: "http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/ha-long-bay/pagePropertiesImage/ha-long-bay.jpg",
+        url: "http://www.vietnam-guide.com/ha-long-bay/",
+        comments: ["comment1", "comment2"],  
+        category: "Places of Interest",
+        trip: //put in the trip id,
+    },
+    {
+        title: "Angsana Lang Co", 
+        description: "Resort", 
+        location: "Central Vietnam", 
+        image: "https://banyantreemedia.s3.amazonaws.com/Uncover-920-x-518-2-CF1F48EB-7E58-4E42-A9F4-C18C2AE08C9D.jpg",
+        url: "https://www.angsana.com/en/vietnam/lang-co-central-vietnam",
         comments: ["comment1", "comment2"],  
         category: "Accomodation",
         trip: //put in the trip id,
     }
-
-
-
-
-
-
 ]
         
 
